c3df5214bd1a772310d64b30493901e437bcc39f
Tomoya Koyanagi <tomk79@gmail.com>
2022-11-06 12:45:12 +0900

    gulp -> webpack

---
+++ webpack.mix.js
@@ -0,0 +1,46 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel applications. By default, we are compiling the CSS
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix
+	.webpackConfig({
+		module: {
+			rules:[
+				{
+					test:/\.twig$/,
+					use:['twig-loader']
+				}
+			]
+		},
+		resolve: {
+			fallback: {
+				"fs": false,
+				"path": false,
+				"crypto": false
+			}
+		}
+	})
+
+
+	// --------------------------------------
+	// table-tag-editor
+	.js('src/broccoli-field-table.js', 'dist/')
+	.sass('src/broccoli-field-table.css.scss', 'dist/broccoli-field-table.css')
+
+	// --------------------------------------
+	// Test contents
+	.js('tests/testdata/htdocs/index_files/main.src.js', 'tests/testdata/htdocs/index_files/main.js')
+
+	// --------------------------------------
+	// Static libs
+	.copyDirectory('vendor/broccoli-html-editor/broccoli-html-editor/client/dist/', 'tests/testdata/htdocs/libs/')
+;
